200e2b1a6b84113936ad3356f03818c5597bcf52
Bohdan Bodnar <bodnarb.04@spst.cz>
2022-10-11 19:02:28 +0200

    Přidejte soubory projektu.

---
+++ RidiciStruktury04.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string pole[12]{ "leden", "unor", "brezen", "duben", "kveten", "cerven", "cervenec", "spren", "zari", "rijen", "listopad", "prosinec" };
+    int volba;
+    cout << "\n1 = Zobrazi mesic, 2 = Zobrazi mesic az do konce roku, 3 = Konec: ";
+    cin >> volba;
+    while (volba != 3)
+    {
+        switch (volba)
+        {
+        case 1:
+        {
+            int cislo;
+            cout << "Zadej cislo mesice: ";
+            cin >> cislo;
+            cout << pole[cislo - 1];
+        }
+        break;
+        case 2:
+        {
+            int m;
+            cout << "Zadej cislo mesice: ";
+            cin >> m;
+            for (int i = m; i <= 12; i++)
+            {
+                cout << pole[i - 1] << endl << endl;
+            }
+
+
+        }
+        break;
+        default:
+        {
+            cout << "Nevybrana moznost";
+        }
+        break;
+        }
+        cout << "\n1 = Zobrazi mesic, 2 = Zobrazi mesic az do konce roku, 3 = Konec: ";
+        cin >> volba;
+    }
+
+}
